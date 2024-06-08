865a423adc37c858821723b65aaf8fe63f2de428
Melan1408 <andreymelnik9517@gmail.com>
2024-06-08 16:03:18 +0300

    Homework9

---
+++ HomeWork9/MauiFirstApp/MauiFirstApp/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiFirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowResume(object sender, EventArgs e)
+        {
+            Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+            string workingDirectory = Directory.GetCurrentDirectory();
+
+            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.Parent.Parent.FullName;
+
+            try
+            {
+                ResumeLabel.Text = File.ReadAllText($"{projectDirectory}/resume.txt");
+            }
+            catch(Exception ex) 
+            {
+                ResumeLabel.Text = ex.Message;
+            }
+
+            SemanticScreenReader.Announce(ResumeLabel.Text);
+        }
+            
+    }
+
+}
